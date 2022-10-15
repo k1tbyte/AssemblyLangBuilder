a9e6e9fdcf3b6ad1f78b059d64735920efd7140e
Explyne <k.egazarov2004@gmail.com>
2022-10-15 19:26:20 +0300

    Добавьте файлы проекта.

---
+++ AsseblerBuildHelper/Config.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsseblerBuildHelper
+{
+    [Serializable]
+    internal class CfgProperties
+    {
+        internal string MASMPath { get; set; }
+        internal string OutputPath { get; set; }
+        internal string LastSrcPath { get; set; }
+        internal bool DontGenObj { get; set; }
+        internal bool ConvertToCp { get; set; }
+        internal bool OpenAssemblyResults { get; set; }
+        internal bool SaveLog { get; set; }
+
+    }
+    internal static class Config
+    {
+        internal static CfgProperties properties { get; set; }
+
+        internal static void Load()
+        {
+            if (properties != null)
+                return;
+
+            if (File.Exists(".\\properties.dat"))
+            {
+                properties = ReadFromBinaryFile<CfgProperties>(".\\properties.dat");
+            }
+            else
+            {
+                properties = new CfgProperties();
+                properties.SaveLog = true;
+                WriteToBinaryFile<CfgProperties>(".\\properties.dat", properties);
+            }
+        }
+
+        internal static void Save()
+        {
+            WriteToBinaryFile(".\\properties.dat", properties);
+        }
+
+        public static void WriteToBinaryFile<T>(string filePath, T objectToWrite, bool append = false)
+        {
+            using (Stream stream = File.Open(filePath, append ? FileMode.Append : FileMode.Create))
+            {
+                var binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                binaryFormatter.Serialize(stream, objectToWrite);
+            }
+        }
+
+        public static T ReadFromBinaryFile<T>(string filePath)
+        {
+            using (Stream stream = File.Open(filePath, FileMode.Open))
+            {
+                var binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                return (T)binaryFormatter.Deserialize(stream);
+            }
+        }
+    }
+}
---
+++ AsseblerBuildHelper/MainWindow.xaml.cs
@@ -0,0 +1,234 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.WebRequestMethods;
+
+namespace AsseblerBuildHelper
+{
+    public partial class MainWindow : Window
+    {
+        private string FileName;
+        private string SrcPath;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Config.Load();
+            if(!String.IsNullOrEmpty(Config.properties.MASMPath))
+                MasmPath.Text = Config.properties.MASMPath;
+            
+            if(!String.IsNullOrEmpty(Config.properties.OutputPath))
+                OutputPath.Text = Config.properties.OutputPath;
+            
+            if (!String.IsNullOrEmpty(Config.properties.LastSrcPath) && System.IO.File.Exists(Config.properties.LastSrcPath))
+            {
+                SrcPath = Config.properties.LastSrcPath;
+                FileName = SelectedText.Text = SrcPath.Split('\\').Last();
+            }
+                
+
+
+            convertEncoding.IsChecked = Config.properties.ConvertToCp;
+            openLogAfter.IsChecked    = Config.properties.OpenAssemblyResults;
+            dontGenObj.IsChecked      = Config.properties.DontGenObj;
+            saveLog.IsChecked         = Config.properties.SaveLog;
+            
+        }
+
+        private void close_Click(object sender, RoutedEventArgs e)
+        {
+            App.Current.Shutdown();
+        }
+
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void Border_Drop(object sender, DragEventArgs e)
+        {
+            var file = ((string[])e.Data.GetData(DataFormats.FileDrop)).FirstOrDefault();
+
+            if (!file.EndsWith(".asm"))
+            {
+                System.Windows.Forms.MessageBox.Show("Select .asm file!");
+                return;
+            }
+            
+
+            FileName = SelectedText.Text = file.Split('\\').Last();
+            SrcPath  = Config.properties.LastSrcPath = file;
+            Config.Save();
+        }
+
+        private long UnixTimeNow()
+        {
+            var timeSpan = (DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0));
+            return (long)timeSpan.TotalSeconds;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(String.IsNullOrEmpty(SrcPath))
+            {
+                System.Windows.Forms.MessageBox.Show("Source file is not selected!");
+                return;
+            }
+            else if(String.IsNullOrEmpty(MasmPath.Text))
+            {
+                System.Windows.Forms.MessageBox.Show("MASM dir is not selected!");
+                return;
+            }
+            else if (!System.IO.File.Exists(SrcPath))
+            {
+                System.Windows.Forms.MessageBox.Show($"File {FileName} is not found!");
+                SelectedText.Text = Config.properties.LastSrcPath = SrcPath = null;
+                Config.Save();
+                return;
+            }
+            else if (!Directory.Exists(MasmPath.Text))
+            {
+                MasmPath.Text = Config.properties.MASMPath = null;
+                Config.Save();
+                System.Windows.Forms.MessageBox.Show("MASM dir is not found!");
+                return;
+            }
+            else if(!String.IsNullOrEmpty(OutputPath.Text) && OutputPath.Text != "Click for select . . ." && !Directory.Exists(OutputPath.Text))
+            {
+                Config.properties.OutputPath = null;
+                OutputPath.Text = "Click for select . . .";
+                Config.Save();
+                System.Windows.Forms.MessageBox.Show("Output dir is not valid!");
+                
+                return;
+
+            }
+
+            var outputPath = (String.IsNullOrWhiteSpace(OutputPath.Text) || OutputPath.Text == "Click for select . . .") ? Directory.GetCurrentDirectory() : OutputPath.Text;
+            var outputFileExe = MasmPath.Text + "\\bin\\" + FileName.Replace(".asm", ".exe");
+            var outputFileObj = outputFileExe.Replace(".exe", ".obj");
+            var objCommand = dontGenObj.IsChecked == true ? $"DEL /F /Q \"{outputFileObj}\"" : $"move /Y \"{outputFileObj}\" \"{outputPath}\"";
+
+            if (convertEncoding.IsChecked == true)
+            {
+                var output = System.IO.File.ReadAllLines(SrcPath);
+                if(System.IO.File.Exists($"{MasmPath.Text}\\bin\\{FileName}"))
+                {
+                    System.IO.File.Delete($"{MasmPath.Text}\\bin\\{FileName}");
+                }
+                System.IO.File.WriteAllLines($"{MasmPath.Text}\\bin\\{FileName}", output, Encoding.GetEncoding(866));
+            }
+            else
+            {
+                System.IO.File.Copy(SrcPath, MasmPath.Text + "\\bin\\"+FileName, true);
+            }
+
+            if ((saveLog.IsChecked == true || openLogAfter.IsChecked == true) && !System.IO.Directory.Exists(".\\Logs"))
+            {
+                System.IO.Directory.CreateDirectory(".\\Logs");
+            }
+
+            var logFile = $".\\Logs\\[{UnixTimeNow()}] {FileName.Replace(".asm", "")}.txt";
+            using (var process = new Process())
+            {
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.FileName = "cmd";
+                process.StartInfo.Arguments =  $"/c cd /d \"{MasmPath.Text}\"\\bin && ml.exe /c /coff \"{FileName}\" && link.exe /SUBSYSTEM:CONSOLE \"{FileName.Replace(".asm",".obj")}\" &&" +
+                    $" move /Y \"{outputFileExe}\" \"{outputPath}\" && {objCommand}";
+                process.StartInfo.RedirectStandardOutput = true;
+                process.Start();
+                var result = process.StandardOutput.ReadToEnd();
+
+                if(saveLog.IsChecked == true || openLogAfter.IsChecked == true)
+                     System.IO.File.WriteAllText(logFile, result);
+                process.WaitForExit();   
+                if(result.Contains("error"))
+                {
+                    System.Windows.Forms.MessageBox.Show(result,"Compile error");
+                }
+            }
+
+
+            if(openLogAfter.IsChecked == true)
+                Process.Start(logFile);
+
+            System.IO.File.Delete(MasmPath.Text + "\\bin\\" + FileName);
+           
+        }
+
+        private void SetMasmDir_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            using (var dialog = new System.Windows.Forms.FolderBrowserDialog())
+            {
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK && !String.IsNullOrEmpty(dialog.SelectedPath))
+                {
+                    MasmPath.Text = dialog.SelectedPath;
+                    Config.properties.MASMPath = dialog.SelectedPath;
+                    Config.Save();
+                }
+            }
+                
+        }
+
+        private void SetOutputPath_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            using (var dialog = new System.Windows.Forms.FolderBrowserDialog())
+            {
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK && !String.IsNullOrEmpty(dialog.SelectedPath))
+                {
+                    OutputPath.Text = dialog.SelectedPath;
+                    Config.properties.OutputPath = dialog.SelectedPath;
+                    Config.Save();
+                }
+            }
+
+        }
+
+        private void convertEncoding_Click(object sender, RoutedEventArgs e)
+        {
+            Config.properties.ConvertToCp = convertEncoding.IsChecked == true;
+            Config.Save();
+        }
+
+        private void dontGenObj_Click(object sender, RoutedEventArgs e)
+        {
+            Config.properties.DontGenObj = dontGenObj.IsChecked == true;
+            Config.Save();
+        }
+
+        private void openLogAfter_Click(object sender, RoutedEventArgs e)
+        {
+            Config.properties.OpenAssemblyResults = openLogAfter.IsChecked == true;
+            Config.Save();
+        }
+
+        private void OpenEditor_Click(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrEmpty(SrcPath) && !System.IO.File.Exists(SrcPath))
+            {
+                System.Windows.Forms.MessageBox.Show($"File {FileName} is not found!");
+                SelectedText.Text = Config.properties.LastSrcPath = SrcPath = null;
+                Config.Save();
+                return;
+            }
+            Process.Start(SrcPath);
+
+        }
+    }
+}
